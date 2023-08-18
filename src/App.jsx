66ce792f980a585d23dfdc207dcf1bb31bf6716d
kaykyveiga <kaykyveiga46@gmail.com>
2023-08-18 17:06:13 -0300

    basic structure created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-
+import {GrFormPrevious, GrFormNext} from "react"
 import './App.css'
 
 function App() {
 
   return (
-    <>
-
-    </>
+   <div className="app">
+    <h2>Deixe sua Avaliação</h2>
+    <p>Agradecemos pela sua compra! Utilize o formulário abaixo para compartilhar a sua avaliação do produto.</p>
+    <p>Etapas</p>
+    <form>
+      <div className="actions">
+        <button>
+          <GrFormPrevious/>
+          <span>Voltar</span>
+        </button>
+        <button>Avançar</button>
+        <GrFormNext/>
+      </div>
+    </form>
+   </div>
   )
 }
 
