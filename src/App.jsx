114c9b996941c39f157b8efa429cf7d3f8557f37
kaykyveiga <kaykyveiga46@gmail.com>
2023-08-22 16:33:14 -0300

    initial styles

--- src/App.jsx
+++ src/App.jsx
@@ -17,17 +17,23 @@ function App() {
   const { currentStep, currentComponent, stepChange, firstStep, lastStep } = useForm(formComponents)
 
   return (
-    <div className="App">
-      <h2>Deixe sua Avaliação</h2>
-      <p>Agradecemos pela sua compra! Utilize o formulário abaixo para compartilhar a sua avaliação do produto.</p>
-      <p>Etapas</p>
-      <form onSubmit={(e) => stepChange(currentStep + 1, e)}>
-        <div className="input-conteiner">{currentComponent}</div>
-        <div className="actions">
-          {!firstStep && (<button type='button' onClick={() => stepChange(currentStep - 1)}>Voltar</button>)}
-          {!lastStep ? (<button type='submit'>Avançar</button>) : (<button type='button'>Enviar</button>)}
-        </div>
-      </form>
+    <div className="app">
+      <div className="header">
+        <h2>Deixe sua Avaliação</h2>
+        <p>Agradecemos pela sua compra! Utilize o formulário abaixo para compartilhar a sua avaliação do produto.</p>
+      </div>
+      <div className="form-conteiner">
+        <p>Etapas</p>
+        <form onSubmit={(e) => stepChange(currentStep + 1, e)}>
+          <div className="input-conteiner">{currentComponent}</div>
+          <div className="actions">
+            {!firstStep && (<button id='back' type='button' onClick={() => stepChange(currentStep - 1)}>Voltar</button>)}
+            {!lastStep ? (<button className='advance' type='submit'>Avançar</button>) : (<button className='advance' type='button'>Enviar</button>)}
+          </div>
+         
+        </form>
+       
+      </div>
     </div>
   )
 }
--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,7 +1,20 @@
 
 const UserForm = () => {
   return (
-    <div>UserForm</div>
+    <div>
+      <div className="form-control">
+        <label>
+          <span>Nome</span>
+          <input type="text" name="name" id="name" required placeholder="Digite seu nome" />
+        </label>
+      </div>
+      <div className="form-control">
+        <label>
+          <span>Email</span>
+          <input type="email" name="email" id="email" required placeholder="Digite seu email" />
+        </label>
+      </div>
+    </div>
   )
 }
 
