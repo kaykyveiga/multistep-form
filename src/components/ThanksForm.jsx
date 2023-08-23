330dd6b3eab68e7a7158a0a987273c5cbb7e07b0
kaykyveiga <kaykyveiga46@gmail.com>
2023-08-23 12:12:57 -0300

    stylized ThanksForm

--- src/components/ThanksForm.jsx
+++ src/components/ThanksForm.jsx
@@ -1,20 +1,20 @@
+//import { BsFillEmojiHeartEyesFill, BsFillEmojiSmileFill, BsFillEmojiNeutralFill, BsFillEmojiFrownFill } from "react-icons/bs"
 
+import "./ThanksForm.css"
 
 const ThanksForm = () => {
   return (
-    <div className="review-form">
-      <div className="thanks-conteiner">
-        <h2>Falta pouco...</h2>
-        <p>A sua opinião é muito importante em breve você reberá um cumpom de desconto de 10% de desconto na sua próxima compra.</p>
-        <p>Para concluir a sua avaliação, clique no botão de Eviar abaixo.</p>
-      </div>
+    <div className="thanks-conteiner">
+      <h2>Falta pouco...</h2>
+      <p>A sua opinião é muito importante em breve você reberá um cumpom de desconto de 10% de desconto na sua próxima compra.</p>
+      <p>Para concluir a sua avaliação, clique no botão de Eviar abaixo.</p>
       <h3>Aqui está o resumo da sua avaliação:</h3>
-      <div className="review-data">
+      <p className="review-data">
         <span>Satisfação com o produto:</span>
-      </div>
-      <div className="review-data">
+      </p>
+      <p className="review-data">
         <span>Comentário:</span>
-      </div>
+      </p>
     </div>
   )
 }
