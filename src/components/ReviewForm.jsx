d2a96a0577ccfb323406032db8f3b7f468b435b9
kaykyveiga <kaykyveiga46@gmail.com>
2023-08-23 10:55:38 -0300

    created evaluation structure

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,8 +1,45 @@
-
+import { BsFillEmojiHeartEyesFill, BsFillEmojiSmileFill, BsFillEmojiNeutralFill, BsFillEmojiFrownFill } from "react-icons/bs"
 
 const ReviewForm = () => {
   return (
-    <div>ReviewForm</div>
+    <div className="review-form">
+      <div className="score-conteiner">
+        <div className="radio-conteiner">
+          <label htmlFor="radio-conteiner">
+            <input type="radio" name="review" required value="unsatisfied" />
+            <BsFillEmojiFrownFill />
+            <p>Insatisfeito</p>
+          </label>
+        </div>
+        <div className="radio-conteiner">
+          <label htmlFor="radio-conteiner">
+            <input type="radio" name="review" required value="neutral" />
+            <BsFillEmojiNeutralFill />
+            <p>Poderia ser melhor</p>
+          </label>
+        </div>
+        <div className="radio-conteiner">
+          <label htmlFor="radio-conteiner">
+            <input type="radio" name="review" required value="satisfied" />
+            <BsFillEmojiSmileFill />
+            <p>Satisfeito</p>
+          </label>
+        </div>
+        <div className="radio-conteiner">
+          <label htmlFor="radio-conteiner">
+            <input type="radio" name="review" required value="very-satisfied" />
+            <BsFillEmojiHeartEyesFill />
+            <p>Muito Satisfeito</p>
+          </label>
+        </div>
+      </div>
+      <div className="form-control">
+        <label>
+          <span>Comentário:</span>
+          <textarea name="coment" cols="30" rows="10" placeholder="Deixe seu comentário aqui..."></textarea>
+        </label>
+      </div>
+    </div>
   )
 }
 
