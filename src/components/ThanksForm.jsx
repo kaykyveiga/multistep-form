ac004d039f0d7164e5c73ae01ccdb7191dcda2ba
kaykyveiga <kaykyveiga46@gmail.com>
2023-08-23 11:53:35 -0300

    created ThanksForm structure

--- src/components/ThanksForm.jsx
+++ src/components/ThanksForm.jsx
@@ -2,7 +2,20 @@
 
 const ThanksForm = () => {
   return (
-    <div>ThanksForm</div>
+    <div className="review-form">
+      <div className="thanks-conteiner">
+        <h2>Falta pouco...</h2>
+        <p>A sua opinião é muito importante em breve você reberá um cumpom de desconto de 10% de desconto na sua próxima compra.</p>
+        <p>Para concluir a sua avaliação, clique no botão de Eviar abaixo.</p>
+      </div>
+      <h3>Aqui está o resumo da sua avaliação:</h3>
+      <div className="review-data">
+        <span>Satisfação com o produto:</span>
+      </div>
+      <div className="review-data">
+        <span>Comentário:</span>
+      </div>
+    </div>
   )
 }
 
