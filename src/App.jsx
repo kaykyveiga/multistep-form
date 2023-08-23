d8024329b478477cbdb8ce4a035955028ce1cc45
kaykyveiga <kaykyveiga46@gmail.com>
2023-08-23 09:54:50 -0300

    created stage view component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import './App.css'
-import ReviewForm from './components/ReviewForm'
-import ThanksForm from './components/ThanksForm'
-import UserForm from './components/UserForm'
-import { useForm } from "./hooks/useForm"
+//Components
+import './App.css';
+import ReviewForm from './components/ReviewForm';
+import ThanksForm from './components/ThanksForm';
+import UserForm from './components/UserForm';
+import Steps from './components/Steps'
 
+//hooks
+import { useForm } from "./hooks/useForm";
 
 function App() {
 
@@ -23,19 +26,18 @@ function App() {
         <p>Agradecemos pela sua compra! Utilize o formulário abaixo para compartilhar a sua avaliação do produto.</p>
       </div>
       <div className="form-conteiner">
-        <p>Etapas</p>
+        <Steps currentStep={currentStep} />
         <form onSubmit={(e) => stepChange(currentStep + 1, e)}>
           <div className="input-conteiner">{currentComponent}</div>
           <div className="actions">
             {!firstStep && (<button id='back' type='button' onClick={() => stepChange(currentStep - 1)}>Voltar</button>)}
             {!lastStep ? (<button className='advance' type='submit'>Avançar</button>) : (<button className='advance' type='button'>Enviar</button>)}
           </div>
-         
         </form>
-       
       </div>
     </div>
   )
 }
+
 
 export default App
---
+++ src/components/Steps.jsx
@@ -0,0 +1,28 @@
+import { AiOutlineUser, AiOutlineStar } from "react-icons/ai";
+import { FiSend } from "react-icons/fi";
+
+
+import "./Steps.css";
+
+/* eslint-disable react/prop-types */
+
+const Steps = ({ currentStep }) => {
+  return (
+    <div className="steps">
+      <div className={`step ${currentStep >= 0 ? "active" : ""}`}>
+        <AiOutlineUser />
+        <p>Identificação</p>
+      </div>
+      <div className={`step  ${currentStep >= 1 ? "active" : ""} `}>
+        <AiOutlineStar />
+        <p>Avaliação</p>
+      </div>
+      <div className={`step ${currentStep >= 2 ? "active" : ""}`}>
+        <FiSend />
+        <p>Envio</p>
+      </div>
+    </div>
+  )
+};
+
+export default Steps
